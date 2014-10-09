84dd4617da9638ebf699222f88359c300e55fc3f
JacksonTeh <jackson_dmc69@hotmail.com>
2014-10-09 18:25:38 +0800

    Added leftRotate(), rightRotate(), leftRightRotate(), rightLeftRotate()

--- src/Rotations.c
+++ src/Rotations.c
@@ -1,1 +1,21 @@
 #include "Rotations.h"
+
+void leftRotate(Node **nodePtr)
+{
+
+}
+
+void rightRotate(Node **nodePtr)
+{
+
+}
+
+void leftRightRotate(Node **nodePtr)
+{
+
+}
+
+void rightLeftRotate(Node **nodePtr)
+{
+
+}
--- test/test_Rotations.c
+++ test/test_Rotations.c
@@ -1,15 +1,57 @@
 #include "unity.h"
+#include "Node.h"
 #include "Rotations.h"
+
+Node node1, node5, node10, node15;
+
+void setNode(Node *target, Node *left, Node *right, char colour)
+{
+    target->left = left;
+    target->right = right;
+    target->colour = colour;
+}
+
+void resetNode(Node *target, int data)
+{
+    target->left = NULL;
+    target->right = NULL;
+    target->colour = 'b';
+    target->data = data;
+}
 
 void setUp(void)
 {
+    resetNode(&node1, 1);
+    resetNode(&node5, 5);
+    resetNode(&node10, 10);
+    resetNode(&node15, 15);
 }
 
 void tearDown(void)
 {
 }
 
-void test_module_generator_needs_to_be_implemented(void)
+/**
+ * root -> 10
+ *         /                5  <- root
+ *        5     --->      /  \
+ *       /               1    10
+ *      1
+ */
+void test_rightRotation(void)
 {
-	TEST_IGNORE_MESSAGE("Implement me!");
+	setNode(&node1, NULL, NULL, 'b');
+	setNode(&node5, &node1, NULL, 'b');
+	setNode(&node10, &node5, NULL, 'b');
+    Node *root = &node10;
+
+    rightRotate(&root);
+
+    TEST_ASSERT_EQUAL_PTR(node1.left, NULL);
+    TEST_ASSERT_EQUAL_PTR(node1.right, NULL);
+    TEST_ASSERT_EQUAL_PTR(node10.left, NULL);
+    TEST_ASSERT_EQUAL_PTR(node10.right, NULL);
+    TEST_ASSERT_EQUAL_PTR(node5.left, &node1);
+    TEST_ASSERT_EQUAL_PTR(node5.right, &node10); 
+    TEST_ASSERT_EQUAL_PTR(root, &node5); 
 }
